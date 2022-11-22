0e2ae459352b99e7bfd243af7cb1472ddc1b6ced
birdflu <birdflu@mail.ru>
2022-11-22 22:35:48 +0300

    feat: full complete generate diagram with default geometry

--- src/draw.rkt
+++ src/draw.rkt
@@ -1,6 +1,31 @@
 (require xml)
 
 (define DIAGRAM_ID "zodcher-diagram")
+(define PAGE_ID "page-1")
+(define GRAPH_MODEL_TAG (string-append
+                         "dx=\"1200\""
+                         "dy=\"900\""
+                         "grid=\"1\""
+                         "gridSize=\"10\""
+                         "guides=\"1\""
+                         "tooltips=\"1\""
+                         "connect=\"1\""
+                         "arrows=\"1\""
+                         "fold=\"1\""
+                         "page=\"1\""
+                         "pageScale=\"1\""
+                         "pageWidth=\"1000\""
+                         "pageHeight=\"800\""
+                         "math=\"0\""
+                         "shadow=\"0\""))
+(define MX_FILE_TAG (string-append
+                     "host=\"app.diagrams.net\""
+                     "modified=\"\""
+                     "agent=\"\""
+                     "etag=\"-lJEG1eWEpPOzzp1d58H\""
+                     "compressed=\"false\""
+                     "version=\"14.6.11\""
+                     "type=\"device\""))
 
 (define (get-element-xml json-element)
   (define (value key)
@@ -26,7 +51,7 @@
        (hash-ref
         (hash "$id" (value 'id)
               "$parent" (if (eq? 'null (value 'idParent))
-                            DIAGRAM_ID
+                            PAGE_ID
                             (value 'idParent))
               "$value" (value 'name)
               "$style" (value 'style)
@@ -52,22 +77,37 @@
 (define (xml-list->string xml-list)
   (string-append* "" xml-list))
 
-(define (add-root-tag xml)
-  (string-append "<root>" xml "</root>"))
+(define (add-graph-model-tag xml)
+  (string-append
+   "<mxGraphModel" GRAPH_MODEL_TAG ">"
+   xml
+   "</mxGraphModel>"))
+
+(define (add-header xml)
+  (string-append "<mxfile " MX_FILE_TAG ">"
+                 "<diagram id=\"" DIAGRAM_ID "\" name=\"Diagram\">"
+                 "<mxGraphModel " GRAPH_MODEL_TAG ">"
+                 "<root>"
+                 xml
+                 "</root>"
+                 "</mxGraphModel>"
+                 "</diagram>"
+                 "</mxfile>"))
 
 (define (add-page-cell xml-list)
-  (cons '"<mxCell id=\"zodcher-diagram\"/>"
-        (cons '"<mxCell id=\"page-1\" parent=\"zodcher-diagram\"/>" xml-list)))
+  (cons (string-append "<mxCell id=\"" PAGE_ID "-p\"/>") 
+        (cons (string-append "<mxCell id=\"" PAGE_ID
+                             "\" parent=\""  PAGE_ID
+                             "-p\"/>")
+              xml-list)))
 
-(define (show data)
+(define (draw data port)
   (display-xml/content
    (xexpr->xml
     (string->xexpr
-     (add-root-tag
+     (add-header
       (xml-list->string
        (add-page-cell
         (get-xml-element-list data))))))
+   port
    #:indentation 'classic))
-
-(define (draw data)
-  (show data))
--- src/main.rkt
+++ src/main.rkt
@@ -1,11 +1,24 @@
+#lang racket
 (include "geometry.rkt")
 (include "draw.rkt")
+
+(define RESULT_PATH "./diagram.xml")
 
 (define (architect data)
   data)
 
+(define out
+  (open-output-file  RESULT_PATH
+                     #:mode 'text	 	 	 	 
+                     #:exists 'append))
+
 (draw
  (architect
   (init-geometry
-              "../data/mushroom.json"
-              "../data/settings.json")))
+   "./data/mushroom.json"
+   "./data/settings.json"))
+ out)
+ 
+(close-output-port out)
+(displayln
+ (string-append "diagram is exported to " RESULT_PATH))
